ad1baa0df141436e28720586858e2f98528a400d
RyuHaJung <red061011@gmail.com>
2024-03-26 17:18:49 +0900

    업데이트

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -27,26 +27,26 @@ public class GameUI : MonoBehaviour
 
     public void UpdateHpUI()
     {
-        HpPoint.text = "HP : " + GameSystem.Instance.statsTalbeOject.statsData.currentHpPoint;
+        HpPoint.text = "HP : " + GameSystem.Instance.stats.currentHpPoint;
     }
 
     public void UpdateSpUI()
     {
-        SpPoint.text = "SP : " + GameSystem.Instance.statsTalbeOject.statsData.currentSpPoint;
+        SpPoint.text = "SP : " + GameSystem.Instance.stats.currentSpPoint;
     }
 
     public void UpdateXpUI()
     {
-        XpPoint.text = "XP : " + GameSystem.Instance.statsTalbeOject.statsData.currentXpPoint;
+        XpPoint.text = "XP : " + GameSystem.Instance.stats.currentXpPoint;
     }
 
     public void UpdateStats()
     {
-        STR.text = "STR : " + GameSystem.Instance.statsTalbeOject.statsData.strength;
-        DEX.text = "DEX : " + GameSystem.Instance.statsTalbeOject.statsData.dexterity;
-        CON.text = "CON : " + GameSystem.Instance.statsTalbeOject.statsData.consitiution;
-        INT.text = "INT : " + GameSystem.Instance.statsTalbeOject.statsData.Intelligence;
-        WIS.text = "WIS : " + GameSystem.Instance.statsTalbeOject.statsData.wisdom;
-        CHA.text = "CHA : " + GameSystem.Instance.statsTalbeOject.statsData.charisma;
+        STR.text = "STR : " + GameSystem.Instance.stats.strength;
+        DEX.text = "DEX : " + GameSystem.Instance.stats.dexterity;
+        CON.text = "CON : " + GameSystem.Instance.stats.consitiution;
+        INT.text = "INT : " + GameSystem.Instance.stats.Intelligence;
+        WIS.text = "WIS : " + GameSystem.Instance.stats.wisdom;
+        CHA.text = "CHA : " + GameSystem.Instance.stats.charisma;
     }
 }
