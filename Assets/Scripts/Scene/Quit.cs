c041408914ef2cb554260ac99cd96a8be0622cb4
RyuHaJung <red061011@gmail.com>
2024-06-05 13:22:46 +0900

    Feat:"종료 추가"

--- Assets/Scripts/Scene/Quit.cs
+++ Assets/Scripts/Scene/Quit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameExit : MonoBehaviour
+public class Quit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class GameExit : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
     }
 }
